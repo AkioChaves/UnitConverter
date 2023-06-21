7dd1f0e36e479d9da3dd28594568a64688bdaeaf
João Chaves <jchaaves95@gmail.com>
2023-06-21 15:12:45 -0300

    Converter made in CSharp

---
+++ UnitConverterCSharp/UnitConverterCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnitConverterCSharp.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Calling the service to convert
+        Converter Conv = new Converter();
+
+        //Program apresentation
+        Console.WriteLine("Unit Converter type:");
+        Console.WriteLine("1) To converte Celsius to Fahrenheit.");
+        Console.WriteLine("2) To converte Fahrenheit to Celsius.");
+        Console.WriteLine("3) To converte Meters to Feet.");
+        Console.WriteLine("4) To converte Feet to Meters.");
+        Console.WriteLine("5) To converte Kilograms to Pounds.");
+        Console.WriteLine("6) To converte Pounds to Kilograms.");
+
+        //Receiver to know what value convert
+        Console.Write("Type 1 to 6: ");
+        int N = int.Parse(Console.ReadLine());
+        Conv.Converting(N);
+    }
+}
---
+++ UnitConverterCSharp/UnitConverterCSharp/Services/Converter.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+
+namespace UnitConverterCSharp.Services
+{
+    //Class responsable to convert the values
+    internal class Converter
+    {
+        //Variable
+        public double Result { get; set; }
+
+        public void Converting(int num)
+        {
+            //Switch to determine what to convert and give the result
+            switch(num)
+            {
+                case 1:
+                    Console.Write("Celsius: ");
+                    double celsius = double.Parse(Console.ReadLine());
+                    Result = celsius * 1.8 + 32;
+                    Console.WriteLine($"Fahrenheit: {Result.ToString("F2", CultureInfo.InvariantCulture)}°F.");
+                    break;
+                case 2:
+                    Console.Write("Fahrenheit: ");
+                    double fahrenheit = double.Parse(Console.ReadLine());
+                    Result = (fahrenheit - 32) / 1.8;
+                    Console.WriteLine($"Celsius: {Result.ToString("F2", CultureInfo.InvariantCulture)}°C.");
+                    break;
+                case 3:
+                    Console.Write("Meters: ");
+                    double meters = double.Parse(Console.ReadLine());
+                    Result = meters * 3.28084;
+                    Console.WriteLine($"Feet: {Result.ToString("F2", CultureInfo.InvariantCulture)}ft.");
+                    break;
+                case 4:
+                    Console.Write("Feet: ");
+                    double feet = double.Parse(Console.ReadLine());
+                    Result = feet / 3.28084;
+                    Console.WriteLine($"Meters: {Result.ToString("F2", CultureInfo.InvariantCulture)}m.");
+                    break;
+                case 5:
+                    Console.Write("Kilograms: ");
+                    double kilograms = double.Parse(Console.ReadLine());
+                    Result = kilograms * 2.20462;
+                    Console.WriteLine($"Pounds: {Result.ToString("F2", CultureInfo.InvariantCulture)}lbs.");
+                    break;
+                case 6:
+                    Console.Write("Pounds: ");
+                    double pounds = double.Parse(Console.ReadLine());
+                    Result = pounds / 2.20462;
+                    Console.WriteLine($"Pounds: {Result.ToString("F2", CultureInfo.InvariantCulture)}Kg.");
+                    break;
+                default:
+                    Console.WriteLine("This value doesn't exist!");
+                    break;
+            }
+        }
+    }
+}
